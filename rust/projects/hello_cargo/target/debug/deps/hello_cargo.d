e99fe8fefef47f4620c5974a6d56112923bd166c
Hawken <HawkenSly@gmail.com>
2021-12-12 05:53:06 -0500

    test

---
+++ rust/projects/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\coding\coding\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+D:\coding\coding\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ rust/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\coding\coding\rust\hello_cargo\target\debug\hello_cargo.exe: D:\coding\coding\rust\hello_cargo\src\main.rs
