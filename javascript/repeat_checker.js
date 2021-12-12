a967321f0c60f5bd167705771bd1c1eb8e77ae20
Hawken <HawkenSly@gmail.com>
2021-12-12 05:51:16 -0500

    adding new code/projects

--- javascript/repeat_checker.js
+++ javascript/repeat_checker.js
@@ -12,4 +12,4 @@ function repeatChecker(characterList) {
     //the .join() modifier joins all of the array into a single string, separated by spaces (' ')
 }
 
-repeatChecker("番輝 星 度 指 折 前 見 小 握 拳 振 返 想 時 数 頃 終 手 開 新 始 運命 時間 相容 絶体 絶命 明日 息 散 花残 香 求 本心 信 救 詩 描 糸 紡 作 我 絆 笑愛 過去 歌 瞬 超 未来 勇気 愛 知 前 欲 目 閉 前")
+repeatChecker("番 輝 星 度 指 折 前 見 小 握 拳 振 返 想 時 数 頃 終 手 開 新 始 運命 時間 相容 絶体 絶命 明日 息 散 花残 香 求 本心 信 救 詩 描 糸 紡 作 我 絆 笑愛 過去 歌 瞬 超 未来 勇気 愛 知 前 欲 目 閉 前")
